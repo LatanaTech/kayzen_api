4a327220a4ba19bfe3263bcc9b034566f5f98943
Jonathan Senior <jonosenior@gmail.com>
2024-02-14 15:27:29 +0100

    make methods private

--- lib/kayzen_api/endpoints/authentication_token.rb
+++ lib/kayzen_api/endpoints/authentication_token.rb
@@ -2,23 +2,28 @@ module KayzenApi
   class AuthenticationToken < Endpoint
     path "authentication/token"
 
-    def self.add_headers(options)
-      body_params = { username: App.config.username, password: App.config.password, grant_type: "password" }
-      base_64_encoded_api_key = Base64.encode64("#{App.config.api_key}:#{App.config.secret_api_key}")
+    class << self
 
-      options[:body] = body_params
-      options[:headers] = (options[:headers] || {}).merge({ "Authorization" => "Basic #{base_64_encoded_api_key}" })
+      private
 
-      super
-    end
+      def add_headers(options)
+        body_params = { username: App.config.username, password: App.config.password, grant_type: "password" }
+        base_64_encoded_api_key = Base64.encode64("#{App.config.api_key}:#{App.config.secret_api_key}")
 
-    def self.handle_response(response)
-      return super unless response.success?
+        options[:body] = body_params
+        options[:headers] = (options[:headers] || {}).merge({ "Authorization" => "Basic #{base_64_encoded_api_key}" })
 
-      oauth_token = JSON.parse(response.body).fetch("access_token")
-      App.config.oauth_token = oauth_token
+        super
+      end
 
-      super
+      def handle_response(response)
+        return super unless response.success?
+
+        oauth_token = JSON.parse(response.body).fetch("access_token")
+        App.config.oauth_token = oauth_token
+
+        super
+      end
     end
   end
 end
