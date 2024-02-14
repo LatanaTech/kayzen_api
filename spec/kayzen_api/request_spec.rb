26b99eb039134e9454f43d873d1581ec5d7f76d5
Jonathan Senior <jonosenior@gmail.com>
2024-02-14 17:37:02 +0100

    begin request spec

---
+++ spec/kayzen_api/request_spec.rb
@@ -0,0 +1,32 @@
+RSpec.describe KayzenApi::Request do
+  class KayzenApi::FakeEndpoint < BeeswaxAPI::Endpoint
+  end
+
+  context 'when the oauth token is present and not about to expire' do
+    let(:oauth_token) { 'fake_oauth_token' }
+
+    before do
+      KayzenApi::App.configure do |config|
+        config.oauth_token = oauth_token
+        config.oauth_token_expires_at = Time.now + 1.hour
+        # config.api_key = api_key
+        # config.secret_api_key = secret_api_key
+        # config.username = username
+        # config.password = password
+      end
+    end
+
+    context 'when the request succeeds' do
+      it 'returns a success response' do
+      end
+    end
+
+    context 'when the request fails' do
+      it 'returns an error response' do
+      end
+    end
+  end
+
+  context 'when the oauth token is about to expire' do
+  end
+end
