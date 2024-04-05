18492ac7a220334a3a5d30ab9a26ddc55faf53d0
Jonathan Senior <jonosenior@gmail.com>
2024-04-05 12:02:01 +0200

    remove extra

--- lib/kayzen_api/endpoints/app_list.rb
+++ lib/kayzen_api/endpoints/app_list.rb
@@ -1,14 +1,5 @@
 module KayzenApi
   class AppList < Endpoint
     path "app_list"
-
-    # This resource is a special case because the URL is plural for the GET method
-    # and singular for the others. We override the target_url method to handle this.
-    # See https://developers.kayzen.io/reference/list-app-lists
-    def self.target_url(options)
-      @path = "app_lists" if options[:method] == :get
-
-      super
-    end
   end
 end
